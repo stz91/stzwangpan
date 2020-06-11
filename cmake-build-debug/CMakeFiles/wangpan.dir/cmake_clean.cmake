589e94d8f76c6d985f079538670a7ebb13689da9
stz91 <657923857@qq.com>
2020-06-11 01:11:40 -0700

    test

---
+++ cmake-build-debug/CMakeFiles/wangpan.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wangpan.dir/main.cpp.o"
+  "wangpan"
+  "wangpan.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wangpan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
